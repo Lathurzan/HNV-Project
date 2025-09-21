af599c6ac69eaf7f2308cd051b29987b931576bf
lathurzan <lathurzan@example.com>
2025-09-21 14:42:05 +0100

    version update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 import { FaQuoteLeft } from "react-icons/fa";
 import HeroSection from "../components/HeroSection";
 import { motion } from "framer-motion";
